04faf09fa78ea7e9a0b8690ab1ffd49c5e1d6ba5
prasadwa <prasad.wanigasinghe@ebuilder.com>
2017-02-10 09:07:56 +0530

    adding multiple

---
+++ src/components/complains/complains.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+class Complains extends React.Component{
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return(
+			<div>
+				Complains
+       		</div>
+       	);
+    }
+
+}
+
+
+export default Complains;
