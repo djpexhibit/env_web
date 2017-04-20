13babf1307227bb4357e500b154b75d67c32afbc
prasadwa <prasad.wanigasinghe@ebuilder.com>
2017-04-20 10:53:56 +0530

    video

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -305,3 +305,34 @@ app.post('/addVideo', function(req, res) {
     });
   });
 });
+
+
+app.post('/getvvv', function (req, res, next) {
+   // var path = config.rootContentFilesPath + '/movie.mp4';
+    var stat = fs.statSync(__dirname + '/files/' + 'name.mp4');
+    var total = stat.size;
+    if (req.headers['range']) {
+        var range = req.headers.range;
+        var parts = range.replace(/bytes=/, "").split("-");
+        var partialstart = parts[0];
+        var partialend = parts[1];
+
+        var start = parseInt(partialstart, 10);
+        var end = partialend ? parseInt(partialend, 10) : total - 1;
+        var chunksize = (end - start) + 1;
+        console.log('RANGE: ' + start + ' - ' + end + ' = ' + chunksize);
+
+        var file = fs.createReadStream(__dirname + '/files/' + 'name.mp4', { start: start, end: end });
+        res.writeHead(206, {
+            'Content-Range': 'bytes ' + start + '-' + end + '/' + total,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunksize,
+            'Content-Type': 'video/mp4'
+        });
+        file.pipe(res);
+    } else {
+        console.log('ALL: ' + total);
+        res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'video/mp4' });
+        fs.createReadStream(__dirname + '/files/' + 'name.mp4').pipe(res);
+    }
+};
