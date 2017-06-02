5ca58537325a4edd72a4b7745386cf5b030ee414
prasadwa <prasad.wanigasinghe@ebuilder.com>
2017-06-02 22:57:36 +0530

    limit query

--- src/models/complainModel.js
+++ src/models/complainModel.js
@@ -13,14 +13,14 @@ function Complain() {
             if(user_id !== 0){
                 con.query(`select c.id as id, p.type as type ,c.res_person as res_person,c.anonymous as anonymous,SUBSTRING(c.details,1,50) as details, `+
                 `DATE_FORMAT(c.date,'%b %d %Y %h:%i %p') as date, u.name as user , i.image as image, u.id as user_id, (select count(*) from comments co where co.complain_id = c.id group by complain_id) as comments `+
-                `from complains c join pollution_type p join user_details u left outer join complain_images i on c.id = i.complain_id and i.selected = 1  where p.id = c.type and c.user_id = u.id order by u.id = ? desc, c.date desc`, user_id, function (err, result) {
+                `from complains c join pollution_type p join user_details u left outer join complain_images i on c.id = i.complain_id and i.selected = 1  where p.id = c.type and c.user_id = u.id order by u.id = ? desc, c.date desc limit 30 `, user_id, function (err, result) {
                 con.release();
                 res.json(result);
                 });
             }else{
                 con.query(`select c.id as id, p.type as type ,c.res_person as res_person,c.anonymous as anonymous,SUBSTRING(c.details,1,50) as details, `+
                 `DATE_FORMAT(c.date,'%b %d %Y %h:%i %p') as date, u.name as user , i.image as image, u.id as user_id, (select count(*) from comments co where co.complain_id = c.id group by complain_id) as comments `+
-                `from complains c join pollution_type p join user_details u left outer join complain_images i on c.id = i.complain_id and i.selected = 1  where p.id = c.type and c.user_id = u.id `, function (err, result) {
+                `from complains c join pollution_type p join user_details u left outer join complain_images i on c.id = i.complain_id and i.selected = 1  where p.id = c.type and c.user_id = u.id limit 30 `, function (err, result) {
                 con.release();
                 //console.log("EEEE");console.log(result)
                 res.json(result);
