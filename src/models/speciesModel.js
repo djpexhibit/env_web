40cdf07a72f159168f780bb0c06751cd470f0d53
prasadwa <prasad.wanigasinghe@ebuilder.com>
2017-07-06 11:06:19 +0530

    species expert

--- src/models/speciesModel.js
+++ src/models/speciesModel.js
@@ -17,6 +17,7 @@ function Species() {
             if(user_id !== 0){
                 con.query(`select s.id as id, s.type as type ,s.name as name,s.anonymous as anonymous,SUBSTRING(s.specname,1,42) as specname, `+
                 `DATE_FORMAT(s.date,'%b %d %Y') as date, u.name as user , i.image as image, u.id as user_id, (select count(*) from species_comments co where co.species_id = s.id group by species_id) as comments, f.is_favorite as fav `+
+                ` s.expert_replied as expertReplied, s.user_replied as userReplied ` +
                 `from species s join user_details u left outer join species_images i on s.id = i.species_id and i.selected = 1 left outer join species_favorite f on s.id = f.species_id and f.user_id = ? and f.is_favorite = 1 where s.user_id = u.id order by u.id = ? desc, s.date desc limit 30 `, [user_id,user_id], function (err, result) {
                 con.release();
                 res.json(result);
@@ -182,16 +183,31 @@ function Species() {
                         });
                     }
 
-                    con.commit(function(err) {
+                    let whichType = 'user_replied';
+                    if(details.type === 'Media' || details.type === 'Expert'){
+                      whichType = 'expert_replied';
+                    }
+
+                    con.query('update species set '+whichType+' = 1 where id = ? ', details.species_id, function(err, result){
                         if (err) {
                             con.rollback(function() {
                               con.release();
-                              res.send({ status: false, message: 'Error' }); return; });
+                              res.send({ status: false, message: 'Error' }); return;
+                            });
                         }
-                        con.release();
-                        res.send({ status: true, message: 'Comment added successfully' });
-                        console.log('success!');
-                    });
+
+                        con.commit(function(err) {
+                            if (err) {
+                                con.rollback(function() {
+                                  con.release();
+                                  res.send({ status: false, message: 'Error' }); return; });
+                            }
+                            con.release();
+                            res.send({ status: true, message: 'Comment added successfully' });
+                            console.log('success!');
+                        });
+
+                      });
 
                 });
             })
