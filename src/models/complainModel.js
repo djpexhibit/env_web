b1e8b2231ef3fc3c256eddac38b0f56d1fda980e
prasadwa <prasad.wanigasinghe@ebuilder.com>
2017-07-09 19:26:23 +0530

    fav chunk

--- src/models/complainModel.js
+++ src/models/complainModel.js
@@ -409,6 +409,31 @@ function Complain() {
         });
     };
 
+    this.loadFavoriteChunk = function (res, user_id, start, end) {
+        connection.acquire(function (err, con) {
+
+            if(user_id !== 0){
+              con.query(`select c.id as id, p.type as type ,c.res_person as res_person,c.anonymous as anonymous,SUBSTRING(c.details,1,42) as details, `+
+              `DATE_FORMAT(c.date,'%b %d %Y %h:%i %p') as date, u.name as user , i.image as image, u.id as user_id, (select count(*) from comments co where co.complain_id = c.id group by complain_id) as comments, `+
+              `c.expert_replied as expertReplied, c.user_replied as userReplied, c.closed as closed, `+
+              ` f.is_favorite as fav from complains c join pollution_type p join user_details u left outer join complain_images i on c.id = i.complain_id and i.selected = 1 join complains_favorite f on c.id = f.complain_id and f.user_id = ?  and f.is_favorite = 1 where p.id = c.type and c.user_id = u.id order by u.id = ? desc, c.date desc limit ?,? `, [user_id,user_id,start,end], function (err, result) {
+              con.release();
+              res.json(result);
+                });
+            }else{
+                con.query(`select c.id as id, p.type as type ,c.res_person as res_person,c.anonymous as anonymous,SUBSTRING(c.details,1,42) as details, `+
+                `c.expert_replied as expertReplied, c.user_replied as userReplied, c.closed as closed, `+
+                `DATE_FORMAT(c.date,'%b %d %Y %h:%i %p') as date, u.name as user , i.image as image, u.id as user_id, (select count(*) from comments co where co.complain_id = c.id group by complain_id) as comments `+
+                `from complains c join pollution_type p join user_details u left outer join complain_images i on c.id = i.complain_id and i.selected = 1  where p.id = c.type and c.user_id = u.id limit 30 `, function (err, result) {
+                con.release();
+                //console.log("EEEE");console.log(result)
+                res.json(result);
+            });
+            }
+
+        });
+    };
+
     this.getFavoriteCount = function (res, user_id) {
         connection.acquire(function (err, con) {
                 con.query(`select count(*) as favCount from complains_favorite f where f.user_id = ? and f.is_favorite = 1 `, [user_id], function (err, result) {
