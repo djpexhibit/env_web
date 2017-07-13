838a8ce073443eb5fe17a316b2057d33c22b69c5
prasadwa <prasad.wanigasinghe@ebuilder.com>
2017-07-13 08:37:24 +0530

    events

--- src/models/eventModel.js
+++ src/models/eventModel.js
@@ -7,7 +7,7 @@ function Event() {
 
     this.loadEvents = function (res) {
         connection.acquire(function (err, con) {
-            con.query('select DATE_FORMAT(s.date,'%b %d %Y') as date, location as location, title as title from events order by date desc',function (err, result) {
+            con.query('select DATE_FORMAT(date,'%b %d %Y') as date, location as location, title as title from events order by date desc',function (err, result) {
                 con.release();
                 res.json(result);
             });
