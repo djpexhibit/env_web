8f458c840a85d28212a913bc5083769d20632e62
prasadwa <prasad.wanigasinghe@ebuilder.com>
2017-07-27 18:15:12 +0530

    limit not included

--- src/models/speciesModel.js
+++ src/models/speciesModel.js
@@ -65,7 +65,7 @@ function Species() {
 console.log(term);
         connection.acquire(function (err, con) {
             if(user_id !== 0){
-                if(term !== ''){
+                if(term){
                   console.log("111");
                   con.query(`select s.id as id, s.type as type ,s.location as location, s.name as name,s.anonymous as anonymous,SUBSTRING(s.specname,1,42) as specname, `+
                   `DATE_FORMAT(s.date,'%b %d %Y') as date, u.name as user , IF(i.image IS NULL, FALSE, TRUE) as image, u.id as user_id, (select count(*) from species_comments co where co.species_id = s.id group by species_id) as comments, f.is_favorite as fav, `+
