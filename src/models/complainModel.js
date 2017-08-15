9476ecd552eccf142e1a2b0811d0b1aaf755108b
prasadwa <prasad.wanigasinghe@ebuilder.com>
2017-08-15 20:33:36 +0530

    test

--- src/models/complainModel.js
+++ src/models/complainModel.js
@@ -145,8 +145,8 @@ function Complain() {
         connection.acquire(function (err, con) {
             con.query(`select c.id as id, p.id as pid, p.type as type, e.id as aid, e.action as action, c.res_person as res_person, c.anonymous as anonymous, c.details as details, i.image as image,`
             +` c.lat as lat, c.lng as lng, DATE_FORMAT(c.date,'%b %d %Y %h:%i %p') as date, u.name as user, u.id as uid, c.location as location, f.is_favorite as fav, concat(pu.name,"-",pu.email) as assignedTo from complains c left outer join complain_images i on  c.id = i.complain_id join pollution_type p join user_details u `
-            + `join expected_action e left outer join complains_favorite f on c.id = f.complain_id and f.user_id = ? and f.is_favorite = 1 join user_details pu where c.id = ? `
-            + ` and p.id = c.type and e.id = c.action and c.user_id = u.id and pu.id = c.assigned_to `, [userId,comp_id] ,function (err, result) {
+            + `join expected_action e left outer join complains_favorite f on c.id = f.complain_id and f.user_id = ? and f.is_favorite = 1 left outer join user_details pu on pu.id = c.assigned_to where c.id = ? `
+            + ` and p.id = c.type and e.id = c.action and c.user_id = u.id  `, [userId,comp_id] ,function (err, result) {
                 con.release();
                 res.json(result);
             });
